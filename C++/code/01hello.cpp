3ffd747a8342e93de3c0b446f68c2a19b7fad3a2
张高境 <491308652@qq.com>
2023-06-27 10:19:44 +0800

    6.27

---
+++ C++/code/01hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+
+    cout << "hello" << endl;
+    // std::cout << "hello" << std::endl;
+
+    system("pause");
+
+    return 0;
+}
