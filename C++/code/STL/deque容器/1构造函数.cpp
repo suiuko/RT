044de51b59e2b2196261cc0a0da50b324ffe6fff
张高境 <491308652@qq.com>
2023-10-16 00:08:25 +0800

    10.16 update

---
+++ C++/code/STL/deque容器/1构造函数.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+#include<deque>
+
+using namespace std;
+
+void printDeque(const deque<int> &d){
+    for(deque<int>::const_iterator it = d.begin();it !=d.end();it++){
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+void test01(){
+    deque<int> d1; //无参构造函数
+    for (int i = 0; i < 10; i++)
+	{
+		d1.push_back(i);
+	}
+	printDeque(d1);
+	deque<int> d2(d1.begin(),d1.end());
+	printDeque(d2);
+
+	deque<int>d3(10,100);
+	printDeque(d3);
+
+	deque<int>d4 = d3;
+	printDeque(d4);
+}
+
+
+int main(){
+    test01();
+
+}
